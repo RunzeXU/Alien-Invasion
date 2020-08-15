a81b90a46619ec28ec73e5653be9145f627c5891
Shenghao XU <52476623+RunzeXU@users.noreply.github.com>
2020-08-15 20:31:25 +0800

    Add files via upload

--- game_functions.py
+++ game_functions.py
@@ -46,12 +46,14 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
 
 
-def update_bullets(bullets):
+def update_bullets(aliens, bullets):
     bullets.update()
 
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
 
 def update_screen(ai_settings, screen, ship, aliens, bullets):
